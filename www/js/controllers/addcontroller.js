099a415a3cc276ff20b66342ad81d89961b74cdb
Mohamed Shebl <mohamed_shebl93@hotmail.com>
2016-05-26 13:15:08 +0200

    commenting on some code parts

--- www/js/controllers/addcontroller.js
+++ www/js/controllers/addcontroller.js
@@ -1,9 +1,14 @@
-app.controller('AddController', ['$scope','$state', function($scope, $state) {
-	// intialize the default value for brand
-	$scope.chosenBrand = 'Samsung';
-	// intialize the default value for memory
-	$scope.chosenMemory = '16GB';
+app.controller('AddController', ['$scope', '$state', function($scope, $state) {
+
 	$scope.mobile = {};
+	// intialize the default value for mobile brand
+	$scope.mobile.brand = 'Samsung';
+	// intialize the default value for mobile memory
+	$scope.mobile.memory = '16GB';
+	// intialize the default value for mobile color
+	$scope.mobile.color = "white";
+	// intialize the default value for mobile screen
+	$scope.mobile.screen = "4";
 	// intialize the default value for mobile options
 	$scope.mobile.options = {
 		dual: false,
@@ -15,13 +20,17 @@ app.controller('AddController', ['$scope','$state', function($scope, $state) {
 	 */
 	$scope.addNewItem = function() {
 		console.log($scope.mobile);
-		var mobilesData = [];
-		//get the cached data of mobiles
-		mobilesData = JSON.parse(localStorage.getItem('data'));
-		//add the new item to the cached data and save it again after adding it
-		mobilesData.push($scope.mobile);
-		localStorage.setItem('data', JSON.stringify(mobilesData));
-		$state.go('home');
+		//mobile model and manufacture year is required to submit
+		if ($scope.mobile.model != null && $scope.mobile.year != null) {
+			var mobilesData = [];
+			//get the cached data of mobiles
+			mobilesData = JSON.parse(localStorage.getItem('data'));
+			//add the new item to the cached data and save it again after adding it
+			mobilesData.push($scope.mobile);
+			localStorage.setItem('data', JSON.stringify(mobilesData));
+			$state.go('home');
+		}
+
 	}
 
 }])
